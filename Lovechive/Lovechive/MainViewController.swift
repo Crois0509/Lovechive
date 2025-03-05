85349d38fc94bb6c09099024aa4f6d4f6130a1f3
Crois0509 <crois0509@naver.com>
2025-03-05 20:10:10 +0900

    ⚒️[proj] 프로젝트 파일 생성

---
+++ Lovechive/Lovechive/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lovechive
+//
+//  Created by 장상경 on 3/5/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
