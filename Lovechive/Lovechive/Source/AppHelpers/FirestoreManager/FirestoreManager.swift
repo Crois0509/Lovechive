c6d5a0cb155a8154a2de130b7c66955c0d9ffc68
Crois0509 <crois0509@naver.com>
2025-03-08 15:23:21 +0900

    ✨[feat] Firestore read 메소드 구현

--- Lovechive/Lovechive/Source/AppHelpers/FirestoreManager/FirestoreManager.swift
+++ Lovechive/Lovechive/Source/AppHelpers/FirestoreManager/FirestoreManager.swift
@@ -64,4 +64,94 @@ final class FirestoreManager {
         }
     }
     
+    func readFromFirestore(type: FirestoreDataTypes, _ completion: @escaping ([String: Any]?) -> Void) {
+        switch type {
+        case .user:
+            let userId = UserDefaults.standard.string(forKey: AppConfig.UserDefaultsConfig.userId) ?? ""
+            let collection = db.collection(type.typeName).document(userId)
+            
+            collection.getDocument { (document, error) in
+                if let error {
+                    debugPrint(error.localizedDescription, "❌ 데이터 읽기 실패")
+                    completion(nil)
+                } else if let document {
+                    if document.exists {
+                        debugPrint("✅ 데이터 불러오기 성공", document.data()!.values)
+                        completion(document.data())
+                    } else {
+                        debugPrint("❌ 데이터가 비어있습니다.")
+                        completion(nil)
+                    }
+                } else {
+                    debugPrint("❌ 데이터를 불러올 수 없습니다.")
+                    completion(nil)
+                }
+            }
+        case .couple:
+            let coupleId = UserDefaults.standard.string(forKey: AppConfig.UserDefaultsConfig.coupleId) ?? ""
+            let collection = db.collection(type.typeName).document(coupleId)
+            
+            collection.getDocument { (document, error) in
+                if let error {
+                    debugPrint(error.localizedDescription, "❌ 데이터 읽기 실패")
+                    completion(nil)
+                } else if let document {
+                    if document.exists {
+                        debugPrint("✅ 데이터 불러오기 성공", document.data()!.values)
+                        completion(document.data())
+                    } else {
+                        debugPrint("❌ 데이터가 비어있습니다.")
+                        completion(nil)
+                    }
+                } else {
+                    debugPrint("❌ 데이터를 불러올 수 없습니다.")
+                    completion(nil)
+                }
+            }
+        case .diary:
+            let diaryId = UserDefaults.standard.string(forKey: AppConfig.UserDefaultsConfig.diaryId) ?? ""
+            let collection = db.collection(type.typeName).document(diaryId)
+            
+            collection.getDocument { (document, error) in
+                if let error {
+                    debugPrint(error.localizedDescription, "❌ 데이터 읽기 실패")
+                    completion(nil)
+                } else if let document {
+                    if document.exists {
+                        debugPrint("✅ 데이터 불러오기 성공", document.data()!.values)
+                        completion(document.data())
+                    } else {
+                        debugPrint("❌ 데이터가 비어있습니다.")
+                        completion(nil)
+                    }
+                } else {
+                    debugPrint("❌ 데이터를 불러올 수 없습니다.")
+                    completion(nil)
+                }
+            }
+        case .schedule:
+            let scheduleId = UserDefaults.standard.string(forKey: AppConfig.UserDefaultsConfig.scheduleId) ?? ""
+            let collection = db.collection(type.typeName).document(scheduleId)
+            
+            collection.getDocument { (document, error) in
+                if let error {
+                    debugPrint(error.localizedDescription, "❌ 데이터 읽기 실패")
+                    completion(nil)
+                } else if let document {
+                    if document.exists {
+                        debugPrint("✅ 데이터 불러오기 성공", document.data()!.values)
+                        completion(document.data())
+                    } else {
+                        debugPrint("❌ 데이터가 비어있습니다.")
+                        completion(nil)
+                    }
+                } else {
+                    debugPrint("❌ 데이터를 불러올 수 없습니다.")
+                    completion(nil)
+                }
+            }
+        }
+    }
+    
+    
 }
