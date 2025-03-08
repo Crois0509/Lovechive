135834531a94dc8795ea7bc96e06ddd9134f6296
Crois0509 <crois0509@naver.com>
2025-03-08 15:02:34 +0900

    ✨[feat] 프로젝트 공용 String 데이터 파일 생성

---
+++ Lovechive/Lovechive/Source/AppHelpers/AppConfig/AppConfig.swift
@@ -0,0 +1,17 @@
+//
+//  AppConfig.swift
+//  Lovechive
+//
+//  Created by 장상경 on 3/8/25.
+//
+
+import Foundation
+
+enum AppConfig {
+    struct UserDefaultsConfig {
+        static let userId: String = "userId"
+        static let coupleId: String = "coupleId"
+        static let diaryId: String = "diaryId"
+        static let scheduleId: String = "scheduleId"
+    }
+}
