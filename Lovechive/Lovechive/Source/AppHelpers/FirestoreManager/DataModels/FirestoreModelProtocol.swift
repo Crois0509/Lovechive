e129b7a76a4642d6638c9ec69b78c9e093c4eb15
Crois0509 <crois0509@naver.com>
2025-03-08 15:02:56 +0900

    ✨[feat] Firestore 데이터 모델 프로토콜 구현

---
+++ Lovechive/Lovechive/Source/AppHelpers/FirestoreManager/DataModels/FirestoreModelProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  FirestoreModelProtocol.swift
+//  Lovechive
+//
+//  Created by 장상경 on 3/8/25.
+//
+
+import Foundation
+
+protocol FirestoreModelProtocol {
+    func transform() -> [String: Any]
+}
