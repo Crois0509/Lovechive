fb823e3256bfd42bbd67cf21acec74aa381d4300
Crois0509 <crois0509@naver.com>
2025-03-09 22:44:50 +0900

    ♻️[refactor] ScheduleDataModel 리팩토링

--- Lovechive/Lovechive/Source/AppHelpers/FirestoreManager/DataModels/ScheduleDataModel.swift
+++ Lovechive/Lovechive/Source/AppHelpers/FirestoreManager/DataModels/ScheduleDataModel.swift
@@ -6,14 +6,25 @@
 //
 
 import Foundation
+import RxDataSources
 import FirebaseFirestore
 
 /// Firestore의 schedules 컬렉션 데이터 모델
-struct ScheduleDataModel: FirestoreModelProtocol {
+struct ScheduleDataModel: FirestoreModelProtocol, IdentifiableType, Equatable {
+    typealias Identity = Int
+    
+    var identity: Identity {
+        return self.date.hashValue
+    }
+    
     let title: String
     let coupleId: String
     let date: Date
     let createdBy: String
+    
+    static func == (lhs: ScheduleDataModel, rhs: ScheduleDataModel) -> Bool {
+        return lhs.identity == rhs.identity
+    }
     
     func transform() -> [String : Any] {
         return [
