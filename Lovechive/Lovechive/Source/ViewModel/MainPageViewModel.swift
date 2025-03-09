4f6cc99978ff64f8310e07d6f7a98647fa464b97
Crois0509 <crois0509@naver.com>
2025-03-09 23:54:08 +0900

    ✨[feat] 메인 페이지 뷰 모델 구현

---
+++ Lovechive/Lovechive/Source/ViewModel/MainPageViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  MainPageViewModel.swift
+//  Lovechive
+//
+//  Created by 장상경 on 3/9/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import FirebaseFirestore
+
+final class MainPageViewModel: ViewModelType {
+    struct Input {
+        let fetchTrigger: PublishRelay<Void>
+    }
+    
+    struct Output {
+        let sections: BehaviorRelay<[PlanTableViewSection]>
+    }
+    
+    private var disposeBag = DisposeBag()
+    
+    private let sections = BehaviorRelay<[PlanTableViewSection]>(value: [])
+    
+    func transform(input: Input) -> Output {
+        input.fetchTrigger
+            .withUnretained(self)
+            .asDriver(onErrorDriveWith: .empty())
+            .drive { owner, _ in
+                owner.fetchData()
+            }.disposed(by: disposeBag)
+        
+        return Output(sections: sections)
+    }
+    
+    private func fetchData() {
+        FirestoreManager.shared.readFromFirestore(type: .schedule, { query in
+            guard let query else { return }
+            
+            let data = query.map {
+                ScheduleDataModel(
+                    title: $0.data()["title"] as? String ?? "",
+                    coupleId: $0.data()["coupleId"] as? String ?? "",
+                    date: ($0.data()["date"] as? Timestamp)?.dateValue() ?? Date(),
+                    createdBy: $0.data()["createdBy"] as? String ?? ""
+                )
+            }
+            
+            let section = PlanTableViewSection(items: data)
+            self.sections.accept([section])
+        })
+    }
+}
