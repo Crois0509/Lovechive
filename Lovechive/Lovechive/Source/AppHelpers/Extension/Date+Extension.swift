f8356dfa36b0c5b4ffcf48b84c015d885da05b45
Crois0509 <crois0509@naver.com>
2025-03-09 22:44:05 +0900

    ✨[feat] 데이트 포매터 구현

--- Lovechive/Lovechive/Source/AppHelpers/Extension/Date+Extension.swift
+++ Lovechive/Lovechive/Source/AppHelpers/Extension/Date+Extension.swift
@@ -11,7 +11,16 @@ extension Date {
     
     func formattedDate() -> String {
         let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
         formatter.dateFormat = "yyyy년 M월 d일"
+        
+        return formatter.string(from: self)
+    }
+    
+    func formattedDateAndTime() -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy년 M월 d일(E) a h시 m분"
         
         return formatter.string(from: self)
     }
