13d67b7eb96bd4e3bcb80abb70231d4a0577ddf6
Crois0509 <crois0509@naver.com>
2025-03-12 18:02:47 +0900

    ✨[feat] 텍스트필드 플레이스홀더 설정 메소드 구현

---
+++ Lovechive/Lovechive/Source/AppHelpers/Extension/UITextField+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UITextField+Extension.swift
+//  Lovechive
+//
+//  Created by 장상경 on 3/12/25.
+//
+
+import UIKit
+
+extension UITextField {
+    /// 텍스트필드의 플레이스홀더를 세팅하는 메소드
+    /// - Parameters:
+    ///   - title: 플레이스홀더의 텍스트
+    ///   - color: 플레이스홀더의 컬러
+    func setPlaceholder(title: String, color: UIColor) {
+        let title = title
+        self.attributedPlaceholder = NSAttributedString(
+            string: title,
+            attributes: [NSAttributedString.Key.foregroundColor: color,
+                         NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .regular)
+                        ]
+        )
+    }
+}
