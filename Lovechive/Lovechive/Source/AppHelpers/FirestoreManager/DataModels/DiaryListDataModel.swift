0c32c27a57763540aec76c4be217d9e056ffca22
Crois0509 <crois0509@naver.com>
2025-03-19 18:44:12 +0900

    ✨[feat] DiaryList 데이터 모델 구현

---
+++ Lovechive/Lovechive/Source/AppHelpers/FirestoreManager/DataModels/DiaryListDataModel.swift
@@ -0,0 +1,40 @@
+//
+//  DiaryListDataModel.swift
+//  Lovechive
+//
+//  Created by 장상경 on 3/19/25.
+//
+
+import Foundation
+import RxDataSources
+import FirebaseFirestore
+
+/// Firestore의 diaries 컬렉션 데이터 모델
+struct DiaryListDataModel: FirestoreModelProtocol, IdentifiableType, Equatable {
+    typealias Identity = String
+    
+    var identity: Identity {
+        return self.diaryId
+    }
+    
+    let diaryId: String
+    let diaryTitle: String
+    let diarySubTitle: String
+    let diaryColor: String
+    let diaryCreatedAt: Date
+    
+    static func == (lhs: DiaryListDataModel, rhs: DiaryListDataModel) -> Bool {
+        return lhs.identity == rhs.identity
+    }
+    
+    func transform() -> [String : Any] {
+        return [
+            AppConfig.DiariesModel.diaryId: self.diaryId,
+            AppConfig.DiariesModel.diaryTitle: self.diaryTitle,
+            AppConfig.DiariesModel.diarySubTitle: self.diarySubTitle,
+            AppConfig.DiariesModel.diaryColor: self.diaryColor,
+            AppConfig.DiariesModel.diaryCreatedAt: self.diaryCreatedAt
+        ]
+    }
+}
+
