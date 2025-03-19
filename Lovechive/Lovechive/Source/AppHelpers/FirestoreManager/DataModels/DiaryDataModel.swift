12d92a5d94878550d492a2c945cdc3b20aafe32d
Crois0509 <crois0509@naver.com>
2025-03-19 18:41:59 +0900

    ♻️[refactor] Diary 모델 RxDataSource 모델화

--- Lovechive/Lovechive/Source/AppHelpers/FirestoreManager/DataModels/DiaryDataModel.swift
+++ Lovechive/Lovechive/Source/AppHelpers/FirestoreManager/DataModels/DiaryDataModel.swift
@@ -6,18 +6,31 @@
 //
 
 import Foundation
+import RxDataSources
 import FirebaseFirestore
 
 /// Firestore의 diaries 컬렉션 데이터 모델
-struct DiaryDataModel: FirestoreModelProtocol {
+struct DiaryDataModel: FirestoreModelProtocol, IdentifiableType, Equatable {
+    typealias Identity = String
+    
+    var identity: Identity {
+        return self.id
+    }
+    
+    let id: String
     let title: String
     let content: String
     let image: String
     let createdAt: Date
     let createdBy: String
     
+    static func == (lhs: DiaryDataModel, rhs: DiaryDataModel) -> Bool {
+        return lhs.identity == rhs.identity
+    }
+    
     func transform() -> [String : Any] {
         return [
+            AppConfig.DiariesModel.id: self.id,
             AppConfig.DiariesModel.title: self.title,
             AppConfig.DiariesModel.content: self.content,
             AppConfig.DiariesModel.image: self.image,
