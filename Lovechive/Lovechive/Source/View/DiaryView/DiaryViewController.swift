df50f6b4653f450e05ab5bc8ca07708b375f3aee
Crois0509 <crois0509@naver.com>
2025-03-23 19:48:11 +0900

    ✨[feat] 다이어리 바인딩 로직 추가

--- Lovechive/Lovechive/Source/View/DiaryView/DiaryViewController.swift
+++ Lovechive/Lovechive/Source/View/DiaryView/DiaryViewController.swift
@@ -12,15 +12,42 @@ import RxCocoa
 import RxDataSources
 
 final class DiaryViewController: UIViewController {
+    typealias DataSource = RxTableViewSectionedAnimatedDataSource<DiariesSection>
     
+    private let collectionView = DiaryCollectionView()
     private let tableView = DiaryTableView()
     private let addButton = FloatingButton()
     
     private var disposeBag = DisposeBag()
     private var viewModel: DiaryViewModel
     private let fetchTrigger = PublishRelay<Void>()
+    fileprivate let updateDiaryData = PublishRelay<Void>()
     
-    private lazy var tableDataSource = TableDataSource(
+    var collectionDataSource: DataSource {
+        let dataSource = DataSource(
+            animationConfiguration:
+                AnimationConfiguration(insertAnimation: .fade,  // 삽입 시 애니메이션
+                                       reloadAnimation: .fade,  // 변경 시 애니메이션 없음
+                                       deleteAnimation: .fade   // 삭제 시 왼쪽으로 사라짐
+                                      ),
+            configureCell: { dataSource, collectionView, indexPath, item in
+                
+                guard let cell = collectionView.dequeueReusableCell(withIdentifier: AppConfig.DiaryConfig.collectionCellId, for: indexPath) as? DiaryCollectionCell else { return .init() }
+                
+                cell.configureCell(item)
+                cell.selectionStyle = .none
+                
+                return cell
+            
+            }, titleForHeaderInSection: { dataSource, index in
+                return dataSource.sectionModels[index].header
+                
+            }, canEditRowAtIndexPath: { _, _ in true })
+        
+        return dataSource
+    }
+    
+    private lazy var tableDataSource = DataSource(
         animationConfiguration:
             AnimationConfiguration(insertAnimation: .fade,  // 삽입 시 애니메이션
                                    reloadAnimation: .fade,  // 변경 시 애니메이션 없음
@@ -50,12 +77,13 @@ final class DiaryViewController: UIViewController {
             
         }, titleForHeaderInSection: { dataSource, index in
             return dataSource.sectionModels[index].header
-        })
+            
+        }, canEditRowAtIndexPath: { _, _ in true })
     
-    init(_ title: String, _ diaryId: String) {
-        self.viewModel = DiaryViewModel(diaryId)
+    init(_ diaryData: DiaryListDataModel) {
+        self.viewModel = DiaryViewModel(diaryData)
         super.init(nibName: nil, bundle: nil)
-        createdNavigationTitle(title)
+        createdNavigationTitle(diaryData.diaryTitle)
     }
     
     required init?(coder: NSCoder) {
@@ -85,18 +113,30 @@ private extension DiaryViewController {
         configureSelf()
         setupLayout()
         bind()
+        
+        let state: String = UserDefaults.standard.string(forKey: "정렬 방법") == nil ? "List" : UserDefaults.standard.string(forKey: "정렬 방법")!
+        if state == "List" {
+            changeCurrentState(.table)
+        } else {
+            changeCurrentState(.collection)
+        }
     }
     
     func configureSelf() {
         view.backgroundColor = .Personal.backgroundPink
         navigationController?.navigationBar.tintColor = .Personal.deepPink
-        [tableView, addButton].forEach {
+        [tableView, collectionView, addButton].forEach {
             view.addSubview($0)
         }
     }
     
     func setupLayout() {
         tableView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.horizontalEdges.equalToSuperview()
+        }
+        
+        collectionView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.bottom.horizontalEdges.equalToSuperview()
         }
@@ -124,15 +164,64 @@ private extension DiaryViewController {
         navigationItem.titleView = titleLabel
     }
     
+    func changeCurrentState(_ type: DiaryState) {
+        UIView.animate(withDuration: 0.3, delay: 0, options: .transitionCrossDissolve) {
+            switch type {
+            case .table:
+                self.collectionView.alpha = 0
+                self.view.insertSubview(self.tableView, aboveSubview: self.collectionView)
+                self.tableView.alpha = 1
+            case .collection:
+                self.tableView.alpha = 0
+                self.view.insertSubview(self.collectionView, aboveSubview: self.tableView)
+                self.collectionView.alpha = 1
+            }
+        }
+    }
+    
     func bind() {
-        let input = DiaryViewModel.Input(fetchTrigger: fetchTrigger)
+        let input = DiaryViewModel.Input(fetchTrigger: fetchTrigger,
+                                         settingButtonTapped: navigationItem.rightBarButtonItem!.rx.tap,
+                                         tableItemSelected: tableView.tableView.rx.itemSelected,
+                                         collectionItemSelected: collectionView.collectionView.rx.itemSelected
+        )
         
         let output = viewModel.transform(input: input)
         
         tableView.tableView.rx.setDelegate(self).disposed(by: disposeBag)
+        collectionView.collectionView.rx.setDelegate(self).disposed(by: disposeBag)
         
         output.sections
             .bind(to: tableView.tableView.rx.items(dataSource: tableDataSource))
+            .disposed(by: disposeBag)
+        
+        output.sections
+            .bind(to: collectionView.collectionView.rx.items(dataSource: collectionDataSource))
+            .disposed(by: disposeBag)
+        
+        output.pushDiaryPage
+            .withUnretained(self)
+            .asSignal(onErrorSignalWith: .empty())
+            .emit { owner, data in
+                debugPrint("다이어리 선택됨", data.title)
+            }
+            .disposed(by: disposeBag)
+        
+        output.sortMethodRelay
+            .withUnretained(self)
+            .asSignal(onErrorSignalWith: .empty())
+            .emit { owner, state in
+                owner.changeCurrentState(state)
+            }
+            .disposed(by: disposeBag)
+        
+        output.fetchDiaryTitle
+            .withUnretained(self)
+            .asSignal(onErrorSignalWith: .empty())
+            .emit { owner, title in
+                owner.createdNavigationTitle(title)
+                owner.updateDiaryData.accept(())
+            }
             .disposed(by: disposeBag)
     }
 }
@@ -165,4 +254,12 @@ extension DiaryViewController: UITableViewDelegate {
         return headerView
     }
     
+}
+
+// MARK: - Reactive Extension
+
+extension Reactive where Base: DiaryViewController {
+    var updateDiaryData: PublishRelay<Void> {
+        base.updateDiaryData
+    }
 }
--- Lovechive/Lovechive/Source/ViewModel/DiaryViewModel.swift
+++ Lovechive/Lovechive/Source/ViewModel/DiaryViewModel.swift
@@ -14,19 +14,30 @@ final class DiaryViewModel: ViewModelMethodManager, ViewModelType {
     
     struct Input {
         let fetchTrigger: PublishRelay<Void>
+        let settingButtonTapped: ControlEvent<Void>
+        let tableItemSelected: ControlEvent<IndexPath>
+        let collectionItemSelected: ControlEvent<IndexPath>
     }
 
     struct Output {
-        let sections: BehaviorRelay<[DiaryTableSection]>
+        let sections: BehaviorRelay<[DiariesSection]>
+        let pushDiaryPage: PublishRelay<DiaryDataModel>
+        let sortMethodRelay: BehaviorRelay<DiaryState>
+        let fetchDiaryTitle: PublishRelay<String>
     }
     
     private var disposeBag = DisposeBag()
     private var diaryId: String
+    private var diaryData: DiaryListDataModel
     
-    private let sections = BehaviorRelay<[DiaryTableSection]>(value: [])
+    private let sections = BehaviorRelay<[DiariesSection]>(value: [])
+    private let pushDiaryPage = PublishRelay<DiaryDataModel>()
+    private let sortMethodRelay = BehaviorRelay<DiaryState>(value: .table)
+    private let fetchDiaryTitle = PublishRelay<String>()
     
-    init(_ diaryId: String) {
-        self.diaryId = diaryId
+    init(_ diaryData: DiaryListDataModel) {
+        self.diaryId = diaryData.diaryId
+        self.diaryData = diaryData
     }
     
     func transform(input: Input) -> Output {
@@ -36,7 +47,7 @@ final class DiaryViewModel: ViewModelMethodManager, ViewModelType {
             .flatMap { owner, _ -> Single<[QueryDocumentSnapshot]> in
                 owner.fetchDiaryData(owner.diaryId)
             }
-            .map { [weak self] query -> [DiaryTableSection] in
+            .map { [weak self] query -> [DiariesSection] in
                 guard let self else { return [] }
                 return self.mappingQueryDataToDiaryData(query)
             }
@@ -46,7 +57,44 @@ final class DiaryViewModel: ViewModelMethodManager, ViewModelType {
             }
             .disposed(by: disposeBag)
         
-        return Output(sections: sections)
+        input.tableItemSelected
+            .withUnretained(self)
+            .compactMap { owner, indexPath -> DiaryDataModel? in
+                owner.searchItem(indexPath)
+            }
+            .asSignal(onErrorSignalWith: .empty())
+            .emit { [weak self] data in
+                self?.pushDiaryPage.accept(data)
+            }
+            .disposed(by: disposeBag)
+        
+        input.collectionItemSelected
+            .withUnretained(self)
+            .compactMap { owner, indexPath -> DiaryDataModel? in
+                owner.searchItem(indexPath)
+            }
+            .asSignal(onErrorSignalWith: .empty())
+            .emit { [weak self] data in
+                self?.pushDiaryPage.accept(data)
+            }
+            .disposed(by: disposeBag)
+        
+        input.settingButtonTapped
+            .withUnretained(self)
+            .flatMap { owner, _ in
+                owner.showSettingAlertView()
+            }
+            .asSignal(onErrorSignalWith: .empty())
+            .emit { [weak self] _ in
+                self?.dismissSettingAlertView()
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(sections: sections,
+                      pushDiaryPage: pushDiaryPage,
+                      sortMethodRelay: sortMethodRelay,
+                      fetchDiaryTitle: fetchDiaryTitle
+        )
     }
 }
 
@@ -57,7 +105,7 @@ private extension DiaryViewModel {
     /// Query 데이터를 DiaryDataModel 타입으로 가공하는 메소드
     /// - Parameter data: Query 데이터
     /// - Returns: 변환된 DiaryDataModel 데이터 배열
-    func mappingQueryDataToDiaryData(_ data: [QueryDocumentSnapshot]) -> [DiaryTableSection] {
+    func mappingQueryDataToDiaryData(_ data: [QueryDocumentSnapshot]) -> [DiariesSection] {
         let data = data.map {
             DiaryDataModel(id: $0.data()[AppConfig.DiariesModel.id] as? String ?? "",
                            title: $0.data()[AppConfig.DiariesModel.title] as? String ?? "",
@@ -66,7 +114,13 @@ private extension DiaryViewModel {
                            createdAt: ($0.data()[AppConfig.DiariesModel.createdAt] as? Timestamp)?.dateValue() ?? Date(),
                            createdBy: $0.data()[AppConfig.DiariesModel.createdBy] as? String ?? "")
         }.sorted(by: {
-            $0.createdAt > $1.createdAt
+            let state: String = UserDefaults.standard.string(forKey: "정렬 순서") == nil ? "최신순" : UserDefaults.standard.string(forKey: "정렬 순서")!
+            
+            if state == "최신순" {
+                return $0.createdAt > $1.createdAt
+            } else {
+                return $0.createdAt < $1.createdAt
+            }
         })
         
         let groupingData = Dictionary(grouping: data) { data in
@@ -74,12 +128,165 @@ private extension DiaryViewModel {
         }
         
         let section = groupingData.map { data in
-            DiaryTableSection(header: data.key, items: data.value)
+            DiariesSection(header: data.key, items: data.value)
         }.sorted {
-            $0.header.formattedStringToDate(.yearMonth) > $1.header.formattedStringToDate(.yearMonth)
+            let state: String = UserDefaults.standard.string(forKey: "정렬 순서") == nil ? "최신순" : UserDefaults.standard.string(forKey: "정렬 순서")!
+            
+            if state == "최신순" {
+                return $0.header.formattedStringToDate(.yearMonth) > $1.header.formattedStringToDate(.yearMonth)
+            } else {
+                return $0.header.formattedStringToDate(.yearMonth) < $1.header.formattedStringToDate(.yearMonth)
+            }
         }
         
         return section
+    }
+    
+    func mappingQueryDataToDiaryListDataModel(_ data: [QueryDocumentSnapshot]) -> DiaryListDataModel? {
+        let data = data.compactMap { data -> DiaryListDataModel? in
+            let item = DiaryListDataModel(coupleId: data.data()[AppConfig.DiariesModel.coupleId] as? String ?? "",
+                                          diaryId: data.data()[AppConfig.DiariesModel.diaryId] as? String ?? "",
+                                          diaryTitle: data.data()[AppConfig.DiariesModel.diaryTitle] as? String ?? "",
+                                          diarySubTitle: data.data()[AppConfig.DiariesModel.diarySubTitle] as? String ?? "",
+                                          diaryColor: data.data()[AppConfig.DiariesModel.diaryColor] as? String ?? "",
+                                          diaryCreatedAt: (data.data()[AppConfig.DiariesModel.diaryCreatedAt] as? Timestamp)?.dateValue() ?? Date())
+            
+            if item.diaryId == "" {
+                return nil
+            } else {
+                return item
+            }
+        }.filter {
+            $0.diaryId == self.diaryId
+        }.first
+        
+        return data
+    }
+    
+    func searchItem(_ indexPath: IndexPath) -> DiaryDataModel? {
+        guard indexPath.section < sections.value.count,
+              indexPath.row < sections.value[indexPath.section].items.count
+        else { return nil }
+        
+        let section = sections.value[indexPath.section]
+        let item = section.items[indexPath.row]
+        
+        return item
+    }
+    
+    func showSettingAlertView() -> Observable<Void> {
+        guard let vc = AppHelpers.getTopViewController(),
+              vc.children.last as? DiarySettingAlertViewController == nil
+        else { return .just(()) }
+        
+        let alert = DiarySettingAlertViewController()
+        
+        vc.addChild(alert)
+        vc.view.addSubview(alert.view)
+        alert.view.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        alert.didMove(toParent: vc)
+        
+        vc.navigationItem.rightBarButtonItem?.isEnabled = false
+        vc.navigationItem.hidesBackButton = true
+        
+        let dismissSignal = alert.rx.deallocated
+        
+        alert.rx.changedSortValue.take(until: dismissSignal)
+            .compactMap { menu -> DiaryState? in
+                if menu == "List" {
+                    return .table
+                } else if menu == "Collection" {
+                    return .collection
+                } else {
+                    return nil
+                }
+            }
+            .asSignal(onErrorSignalWith: .empty())
+            .emit { [weak self] state in
+                self?.sortMethodRelay.accept(state)
+            }
+            .disposed(by: disposeBag)
+        
+        alert.rx.changedSortOrderValue.take(until: dismissSignal)
+            .withUnretained(self)
+            .asSignal(onErrorSignalWith: .empty())
+            .emit { owner, sort in
+                owner.sortedSection(sort)
+            }
+            .disposed(by: disposeBag)
+        
+        alert.rx.editButtonTapped.take(until: dismissSignal)
+            .withUnretained(self)
+            .flatMap { owner, _ -> PublishRelay<Bool> in
+                owner.dismissSettingAlertView()
+                
+                return owner.showAlertView(type: .editDiary(data: owner.diaryData))
+            }
+            .flatMap { [weak self] isSuccess -> Single<[QueryDocumentSnapshot]> in
+                guard let self else { return .just([]) }
+                
+                if isSuccess {
+                    self.dismissAlertView() 
+                    return self.fetchData(.diary(id: self.diaryId))
+                } else {
+                    self.dismissAlertView()
+                    return .just([])
+                }
+            }
+            .compactMap { [weak self] query -> DiaryListDataModel? in
+                self?.mappingQueryDataToDiaryListDataModel(query)
+            }
+            .asSignal(onErrorSignalWith: .empty())
+            .emit { [weak self] data in
+                debugPrint("✅ 다이어리 데이터 편집 성공")
+                self?.fetchDiaryTitle.accept(data.diaryTitle)
+            }
+            .disposed(by: disposeBag)
+        
+        return alert.rx.cancelButtonTapped.take(until: dismissSignal).asObservable()
+    }
+    
+    func dismissSettingAlertView() {
+        guard let topVC = AppHelpers.getTopViewController() as? DiaryViewController,
+              let alert = topVC.children.last as? DiarySettingAlertViewController
+        else { return }
+        
+        alert.dismissSelf {
+            alert.view.snp.removeConstraints()
+            alert.view.removeFromSuperview()
+            alert.removeFromParent()
+        }
+        
+        topVC.navigationItem.rightBarButtonItem?.isEnabled = true
+        topVC.navigationItem.hidesBackButton = false
+    }
+    
+    func sortedSection(_ option: String) {
+        let section = sections.value
+        
+        if option == "최신순" {
+            let sortedSection = section.map {
+                let items = $0.items.sorted(by: { $0.createdAt > $1.createdAt })
+                return DiariesSection(header: $0.header, items: items)
+            }.sorted {
+                $0.header.formattedStringToDate(.yearMonth) > $1.header.formattedStringToDate(.yearMonth)
+            }
+            
+            sections.accept(sortedSection)
+                        
+        } else if option == "오래된순" {
+            let sortedSection = section.map {
+                let items = $0.items.sorted(by: { $0.createdAt < $1.createdAt })
+                return DiariesSection(header: $0.header, items: items)
+            }.sorted {
+                $0.header.formattedStringToDate(.yearMonth) < $1.header.formattedStringToDate(.yearMonth)
+            }
+            
+            sections.accept(sortedSection)
+            
+        }
     }
     
 }
