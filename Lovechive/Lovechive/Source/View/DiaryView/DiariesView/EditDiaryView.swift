cd120e38da338e12c8cb9183464c93c2a47565c5
Crois0509 <crois0509@naver.com>
2025-03-25 16:31:19 +0900

    ♻️[feat] 다이어리 수정 로직 수정

--- Lovechive/Lovechive/Source/View/DiaryView/DiariesView/EditDiaryView.swift
+++ Lovechive/Lovechive/Source/View/DiaryView/DiariesView/EditDiaryView.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 final class EditDiaryView: UIView {
     
-    private let editImageButton = UIButton()
-    private let editDateView = AlertMyTextFieldView(.calendar, title: "작성일자", text: "날짜를 선택해 주세요")
-    private let titleView = AlertMyTextFieldView(.limit(value: 10), title: "제목", text: "일기의 제목을 입력해 주세요")
-    private let contentsView = EditMyTextFieldView(title: "내용", text: "내용을 입력해 주세요")
+    fileprivate let editImageButton = UIButton()
+    fileprivate let imageView = UIImageView()
+    fileprivate let editDateView = AlertMyTextFieldView(.calendar, title: "작성일자", text: "날짜를 선택해 주세요")
+    fileprivate let titleView = AlertMyTextFieldView(.limit(value: 10), title: "제목", text: "일기의 제목을 입력해 주세요")
+    fileprivate let contentsView = EditMyTextFieldView(title: "내용", text: "내용을 입력해 주세요")
+    
+    fileprivate let sendImageData = PublishRelay<UIImage>()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,9 +30,22 @@ final class EditDiaryView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configureDiary(_ data: DiaryDataModel) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        endEditing(true)
+    }
+    
+    func insertImage(_ image: UIImage) {
+        setupImage(image)
+        sendImageData.accept(image)
+    }
+    
+    func configureDiary(_ data: DiaryDataModel?) {
+        guard let data else { return }
         let image = ImageManager.shared.loadImage(path: data.image)
         
+        imageView.image = image
         editDateView.configureTextField(data.createdAt.formattedDateToString(.yearMonthDay))
         titleView.configureTextField(data.title)
         contentsView.configureTextField(data.content)
@@ -41,6 +59,7 @@ private extension EditDiaryView {
     
     func setupUI() {
         setupButton()
+        setupImageView()
         configureSelf()
         setupLayout()
     }
@@ -48,7 +67,7 @@ private extension EditDiaryView {
     func configureSelf() {
         backgroundColor = .white
         layer.cornerRadius = 16
-        [editImageButton, editDateView, titleView, contentsView].forEach {
+        [editImageButton, imageView, editDateView, titleView, contentsView].forEach {
             addSubview($0)
         }
     }
@@ -56,11 +75,16 @@ private extension EditDiaryView {
     func setupLayout() {
         editImageButton.snp.makeConstraints {
             $0.top.horizontalEdges.equalToSuperview().inset(16)
-            $0.height.greaterThanOrEqualTo(80)
+            $0.height.equalTo(80)
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(80)
         }
         
         editDateView.snp.makeConstraints {
-            $0.top.equalTo(editImageButton.snp.bottom).offset(16)
+            $0.top.equalTo(imageView.snp.bottom).offset(16)
             $0.horizontalEdges.equalToSuperview().inset(16)
             $0.height.equalTo(64)
         }
@@ -85,10 +109,60 @@ private extension EditDiaryView {
         editImageButton.setTitleColor(.Personal.highlightPink, for: .normal)
         editImageButton.titleLabel?.font = .myoyaFont(12)
         editImageButton.titleLabel?.textAlignment = .center
+        editImageButton.semanticContentAttribute = .spatial
         editImageButton.backgroundColor = .clear
         editImageButton.layer.cornerRadius = 8
         editImageButton.layer.borderColor = UIColor.Personal.highlightPink.cgColor
         editImageButton.layer.borderWidth = 1
     }
     
+    func setupImageView() {
+        imageView.contentMode = .scaleToFill
+        imageView.backgroundColor = .clear
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 8
+        imageView.layer.borderColor = UIColor.Personal.highlightPink.cgColor
+        imageView.layer.borderWidth = 1
+    }
+    
+    func setupImage(_ image: UIImage) {
+        DispatchQueue.main.async {
+            let aspectRatio = image.size.height / image.size.width
+            let newHeight = self.imageView.frame.width * aspectRatio
+            
+            self.imageView.image = image
+            self.imageView.snp.updateConstraints {
+                $0.height.equalTo(newHeight)
+            }
+            
+            self.editImageButton.snp.updateConstraints {
+                $0.height.equalTo(newHeight)
+            }
+        }
+    }
+    
+}
+
+// MARK: - Reactive Extension
+
+extension Reactive where Base: EditDiaryView {
+    var editImageButtonTapped: ControlEvent<Void> {
+        base.editImageButton.rx.tap
+    }
+    
+    var sendImageData: PublishRelay<UIImage> {
+        base.sendImageData
+    }
+    
+    var sendCreatedDate: ControlProperty<String> {
+        base.editDateView.rx.editingTextField
+    }
+    
+    var sendTitle: ControlProperty<String> {
+        base.titleView.rx.editingTextField
+    }
+    
+    var sendContent: ControlProperty<String> {
+        base.contentsView.rx.editingTextField
+    }
 }
